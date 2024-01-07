c67754f7264ffeb4ee72b8363018c2e90bc9d2b4
yachkuch <yachkuch@gmail.com>
2024-01-08 01:29:36 +0300

    without_tsset

--- test_version.cpp
+++ test_version.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock-actions.h>
+//#include <gmock/gmock-actions.h>
 
 
 int main(int argc, char **argv)
 {
-    testing::InitGoogleTest(&argc, argv);
+    //testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+    //return 0;
 }
